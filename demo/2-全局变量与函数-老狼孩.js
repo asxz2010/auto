667eb1593bbf2a167b185ad1c49e68fb6eb58149
xjj <768373460@qq.com>
2021-03-11 17:38:30 +0800

    3.11

--- demo/2-全局变量与函数-老狼孩.js
+++ demo/2-全局变量与函数-老狼孩.js
@@ -1,7 +1,33 @@
-sleep(1000)
+// sleep(1000)
 
-currentPackage()    // 最近一次监测到的正在运行的应用的包名
+// currentPackage()    // 最近一次监测到的正在运行的应用的包名
 
-currentActivity()   // 最近一次监测到的正在运行的Activity的名称
+// currentActivity()   // 最近一次监测到的正在运行的Activity的名称
+
+// log(currentActivity())
+// log(currentPackage())
+
+// setClip("一个样子")
+// log(getClip())
+
+// toastLog(11111)
+// log(currentActivity())
+// waitForActivity("com.tencent.mm.ui.LauncherUI")
+// toastLog(currentPackage())
+// waitForPackage("com.android.browser")
+// toastLog(2222222)
+
+// exit()
+
+// toastLog(random(1,3))   // 随机整数1、2、3
+// toastLog(random())  // 0~1之间的浮点数
+
+// toastLog(requiresApi(22))
+
+// toastLog(app.autojs.versionCode)
+// toastLog(app.autojs.versionName)
+// toastLog(requiresAutojsVersion("4.1.0 Alpha5"))
+
+runtime.requestPermissions(["access_fine_location"])    //请求GPS权限
 
 
---
+++ demo/3-控制台-老狼孩.js
@@ -0,0 +1,43 @@
+
+console.show()  // 显示控制台
+// log(111111)
+// sleep(3000)
+// console.clear()
+// console.hide()
+
+// console.verbose(11111111)
+// console.log(2222222222)
+// console.info(11111111)
+// console.warn(11111111)
+// console.error(11111111) // 优先级：error > warn > info > log > verbose
+
+// console.assert(2==1, "假的")    // 为 2==1 为false，则输出“假的”
+
+// console.time("求")
+// var sum = 0
+// for(let i=0; i<=10000; i++){
+//     sum += i
+// }
+// console.log(sum)
+// console.timeEnd("求")   // 计算运行时间
+
+// console.trace('Show me')    // 输出文件名和此代码所在的行数
+
+// var n = console.input("请输入一个数字:"); //输入123之后：
+// toast(n + 1);//显示124
+
+// var n = console.rawInput("请输入一个数字:"); //输入123之后：
+// toast(n + 1); //显示1231
+
+// console.show()
+// console.setSize(device.width / 2, device.height / 2)    //设置控制台大小为屏幕的四分之一
+
+// console.setPosition(100, 50)    // 设置控制台的位置
+
+// console.setGlobalLogConfig({    // 设置日志保存的路径和配置
+//     "file": "/sdcard/1.txt"
+// })
+
+
+
+
