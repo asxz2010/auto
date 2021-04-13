a62011df7647ecb749612ff13ae710ad82c7968e
xjj <768373460@qq.com>
2021-04-13 17:59:06 +0800

    4.13

--- demo/protice.js
+++ demo/protice.js
@@ -1,23 +1,20 @@
 var Utils= require('Utils.js')
 
-// launchApp('百度贴吧')
+launchApp('百度贴吧')
 
-// var temp
-// temp = true
+var temp
+temp = true
 
-// sleep(4000)
-// while(temp){
-//     sleep(200)
-//     if(text('搜索').findOne()){
-//         log('首页界面-成功')
-//         let mine = text('我的').findOne()
-//         click(mine.bounds().centerX(),mine.bounds().centerY())
-//         temp = false
-//     }
-// }
+sleep(4000)
+while(temp){
+    sleep(200)
+    if(text('搜索').findOne()){
+        log('首页界面-成功')
+        let mine = text('我的').findOne()
+        click(mine.bounds().centerX(),mine.bounds().centerY())
+        temp = false
+    }
+}
 
-console.log(Utils.getBaiduWords('high'))
-
-// log(Utils.clickBaiduWord('晓九玖'))
 
 
