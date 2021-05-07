7a15025c844e9794fcfec2a595e1d060d5e753b5
xjj <768373460@qq.com>
2021-05-07 21:18:53 +0800

    5.7

--- other.js
+++ other.js
@@ -2,4 +2,4 @@
 let abstract_UI = id('thread_card_abstract').findOnce()
 log(abstract_UI.text().length)
 log(abstract_UI.text())
-log(abstract_UI.text().substring(0,10))
+log(abstract_UI.text().substring(0,100))
